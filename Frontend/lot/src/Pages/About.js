25909017fe6f6cd3fa618a5c42660fc856785336
Meghana <ganeshmeghana2@gmail.com>
2023-01-03 13:45:46 +0530

    commit

--- Frontend/lot/src/Pages/About.js
+++ Frontend/lot/src/Pages/About.js
@@ -3,25 +3,35 @@ import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.css";
 import { useStateDispatch, useTheState } from '../Context';
 import { useNavigate } from "react-router-dom";
+import map from './map.png';
+import logo from './logo.png'
 
 const About=()=>{
 
     return(
         //HTML code here(Use inline CSS and bootstrap)
-        <div className="main" style={{backgroundColor:"black",height:"150%",overflow:"hidden"}}>
+        <div className="main" style={{backgroundColor:"black",height:"100vh",overflow:"hidden",position:"relative"}}>
+            <div>
             <section style={{backgroundColor:"indigo",height:"50%",padding:"1%"}}>
-                <a href="map.png" style={{paddingLeft:"60%",paddingRight:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Home</a>
-                <a href="map.png" style={{padding:"2%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>About</a>
-                <a href="map.png" style={{padding:"2%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Itinerary</a>
-                <a href="map.png" style={{padding:"2%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Committees</a>
-                <a href="map.png" style={{padding:"2%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Profile</a>
+                <img align="left" src={logo} style={{width:"3%",height:"3%"}}></img>
+                <a href="www.google.com" style={{paddingLeft:"60%",paddingRight:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Home</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>About</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Itinerary</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Committees</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Profile</a>
             </section>
+            </div>
+
+        <div>
         <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"center",paddingTop:"5%",fontSize:"150%",fontWeight:"bold"}}>ABOUT</p>
         <p style={{color:"orange",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"left",paddingLeft:"5%",fontWeight:"bold",fontSize:"100%"}}>About us</p>
         <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"left",paddingLeft:"5%",fontWeight:"bold"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Amet necessitatibus non porro alias, eius doloremque eveniet id deserunt<br/> voluptate cumque perspiciatis earum eos obcaecati tempore debitis expedita velit fugiat. Delectus?</p>
         <p style={{color:"orange",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"5%",fontWeight:"bold",fontSize:"100%"}}>Venue</p>
         <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"5%",fontWeight:"bold"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Amet necessitatibus non porro alias, eius doloremque eveniet id deserunt<br/> voluptate cumque perspiciatis earum eos obcaecati tempore debitis expedita velit fugiat. Delectus?</p>
-        <img src="map.png" align="right" height="200px" width="75%"></img>
+        </div>
+        <div>
+        <img src={map} align="right" height="50%" width="60%" style={{paddingRight:"5%"}}></img>
+        </div>
         </div>
         
     );
--- Frontend/lot/src/Pages/Landing.js
+++ Frontend/lot/src/Pages/Landing.js
@@ -3,6 +3,14 @@ import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.css";
 import { useStateDispatch, useTheState } from '../Context';
 import { useNavigate } from "react-router-dom";
+import bg from './bg.png';
+import logo from './logo.png';
+import mun from './mun.png';
+import date from './date.png';
+
+
+
+
 
 const Home=()=>{
 
@@ -14,25 +22,55 @@ const Home=()=>{
 
 
 
-    useEffect(()=>{
-        const checkUser=async()=>{
-            axios.get("/api/user/home",{token:context.token}).then((res)=>{
-                console.log(res.data);
-                console.log("usecheck")
+    // useEffect(()=>{
+    //     const checkUser=async()=>{
+    //         axios.get("/api/user/home",{token:context.token}).then((res)=>{
+    //             console.log(res.data);
+    //             console.log("usecheck")
 
-            }).catch(err=>{
-                console.log("error occured " +err);
-                navigate("/signIn")
-            });
-        }
-        checkUser();
-    },[]);
+    //         }).catch(err=>{
+    //             console.log("error occured " +err);
+    //             navigate("/signIn")
+    //         });
+    //     }
+    //     checkUser();
+    // },[]);
 
     return(
         //HTML code here(Use inline CSS and bootstrap)
-        <div className="main" style={{backgroundImage:"bg.png",height:"200px",width:"500px"}}>
+        <div className="main" style={{backgroundImage:`url(${bg})`,height:"100vh"}}>
+
+            <div>
+            <section style={{backgroundColor:"indigo",height:"50%",padding:"1%"}}>
+                <img align="left" src={logo} style={{width:"3%",height:"3%"}}></img>
+                <a href="www.google.com" style={{paddingLeft:"60%",paddingRight:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Home</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>About</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Itinerary</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Committees</a>
+                <a href="www.google.com" style={{padding:"1%",outline:"none",border:"none",fontWeight:"bold",cursor:"pointer",textDecorationThickness:"30%",textDecorationColor:"orange",color:"white"}}>Profile</a>
+            </section>
+            </div>
 
 
+            <div>
+                <img src={logo} style={{height:"15%",width:"15%",padding:"2%"}}></img>
+            </div>
+
+
+            <div>
+                <img src={date} style={{padding:"1%"}}></img>
+            </div>
+
+
+            <div>
+                <button onClick={"www.google.com"} style={{border:"none",color:"white",backgroundColor:"indigo",fontSize:"150%"}}>Register Now</button>
+            </div>
+
+
+            <div>
+                <img src={mun} style={{height:"100%",width:"100%",padding:"3%"}}></img>
+            </div>
+            
         </div>
 
 
