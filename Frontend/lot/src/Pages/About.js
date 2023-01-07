819773e8e8426ab26a49557ceafa338ffd745af7
Meghana <ganeshmeghana2@gmail.com>
2023-01-07 11:22:36 +0530

    commit

--- Frontend/lot/src/Pages/About.js
+++ Frontend/lot/src/Pages/About.js
@@ -11,7 +11,7 @@ const About=()=>{
     return(
         //HTML code here(Use inline CSS and bootstrap)
         <div className="main" style={{backgroundColor:"black",height:"100vh",overflow:"hidden",position:"relative"}}>
-            <div className="container-fluid" style={{ backgroundColor: '#181818', display: 'grid', height: 750, gridTemplateColumns: '50% 10% 50%', gridTemplateRows: '7.84% 92.16%' }}>
+            <div className="container-fluid" style={{ backgroundColor: '#181818', display: 'grid', height: 750, gridTemplateColumns: '50% 10% 40%', gridTemplateRows: '7.84% 92.16%' }}>
             <div className='navbar' style={{ display: 'grid', backgroundColor: '#1E0B39', gridColumn: '1 /span 3', gridTemplateColumns: '7.5% 92.5%', gridTemplateRows: '100%' }}>
 
             <div className='image'>
@@ -19,17 +19,18 @@ const About=()=>{
             </div>
 
             <div className='tabs'>
-                <ul style={{ display: 'flex', justifyContent: 'flex-end', placeSelf: 'center', listStyle: 'none' }}>
-                    <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Home</a>
-                    <a href="www.google.com" style={{
-                        padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "white", textDecoration: 'underline', textDecorationColor: "#FBBC05",
-                        textDecorationThickness: 3, textShadow: '0px 0px 5px #FBBC05', letterSpacing: '0.02em'
-                    }}>About</a>
-                    <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Itinerary</a>
-                    <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Committees</a>
-                    <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Profile</a>
-                </ul>
+                        <ul style={{ display: 'flex', justifyContent: 'flex-end', placeSelf: 'center', listStyle: 'none' }}>
+                            <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Home</a>
+                            <a href="www.google.com" style={{
+                                padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", textDecoration: 'underline', textDecorationColor: "#FBBC05",
+                                textDecorationThickness: 3, textShadow: '0px 0px 5px #FBBC05', letterSpacing: '0.02em'
+                            }}>About</a>
+                            <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Itinerary</a>
+                            <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Committees</a>
+                            <a href="www.google.com" style={{ padding: "3% 2.5% 2% 2.5%", fontFamily: 'Plus Jakarta Sans', color: "#FFFFFF", letterSpacing: '0.02em',textDecoration:"none"}}>Profile</a>
+                        </ul>
             </div>
+           
             </div>
             <div>
             <p style={{color:"orange",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"left",paddingLeft:"10%",paddingTop:"15%",fontWeight:"bold",fontSize:"100%"}}>About us</p>
@@ -39,9 +40,9 @@ const About=()=>{
                 <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",fontSize:"200%",paddingTop:"20%",fontWeight:"bold",textAlign:"center",paddingLeft:"20%"}}>ABOUT</p>
             </div>
             <div>
-            <p style={{color:"orange",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"30%",paddingTop:"30%",fontWeight:"bold",fontSize:"100%"}}>Venue</p>
-            <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"30%",fontWeight:"bold",fontSize:"80%"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.Amet necessitatibus non porro alias, eius doloremque eveniet id deserunt<br/> voluptate cumque perspiciatis earum eos obcaecati tempore debitis expedita velit fugiat. Delectus?</p>
-            <img src={map} align="right" height="10%" width="110%" style={{paddingRight:"30%"}}></img>
+            <p style={{color:"orange",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"15%",paddingTop:"30%",fontWeight:"bold",fontSize:"100%"}}>Venue</p>
+            <p style={{color:"white",fontFamily:"Arial, Helvetica, sans-serif",textAlign:"right",paddingRight:"15%",fontWeight:"bold",fontSize:"80%"}}>Lorem ipsum dolor sit amet consectetur adipisicing elit.Amet necessitatibus non porro alias, eius doloremque eveniet id deserunt<br/> voluptate cumque perspiciatis earum eos obcaecati tempore debitis expedita velit fugiat. Delectus?</p>
+            <img src={map} align="right" height="10%" width="110%" style={{paddingRight:"15%"}}></img>
 
             </div>
         <div>
